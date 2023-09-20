0fc3827ccf74c67bcbb1cf52e4d26b18ca7e405f
kylefaris <kyle.faris2021@gmail.com>
2023-09-19 21:55:51 -0500

    Workable Form Issue #8

--- Frontend-CB/src/App.tsx
+++ Frontend-CB/src/App.tsx
@@ -1,10 +1,28 @@
+import { useState } from "react";
 import "./App.css";
+
 
 function App() {
   return (
     <>
-      <p> blank page</p>
+      <Credential/>
     </>
+  );
+}
+
+const Credential = () => {
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+  const onSubmitHandler = () => {
+    let formData = [userName, password]
+    console.log(formData) 
+  }
+  return (
+    <div>
+      <input type="text" value={userName} onChange={(e) => {setUserName(e.target.value)}} className="input input-primary" placeholder="Username"/>
+      <input type="password" value={password} onChange={(e) => {setPassword(e.target.value)}}className="input input-primary" placeholder="Password"/>
+      <button onClick={onSubmitHandler} className="btn btn-primary">Login</button>
+    </div>
   );
 }
 
