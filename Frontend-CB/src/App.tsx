fd4412fa18726d1c337afc901d2e5f8e6cbdcfa5
Ian Worley <ian@ianworley.xyz>
2023-09-26 11:01:05 -0500

    modified:   Frontend-CB/src/App.tsx

--- Frontend-CB/src/App.tsx
+++ Frontend-CB/src/App.tsx
@@ -34,9 +34,6 @@ const schema = yup.object().shape({
 	userName: yup.string().required("Username is required"),
 	password: yup.string().required("Password is required"),
 })
-	userName: yup.string().required("Username is required"),
-	password: yup.string().required("Password is required"),
-})
 
 const Credential = () => {
 	const {
@@ -108,19 +105,13 @@ const Credential = () => {
 							Username
 						</label>
 
-						<input
-							required
+				
 						<input
 							required
 							{...register("userName", { required: true })}
 							type="text"
 							id="username"
-							id="username"
 							className={`bg-slate-200 border-b-2 border-slate-600 rounded-sm ${
-								errors.userName ? "border-red-500" : ""
-							}`}
-							//"bg-slate-200 border-b-2 border-slate-600 rounded-sm"
-							placeholder="Enter your username"
 								errors.userName ? "border-red-500" : ""
 							}`}
 							//"bg-slate-200 border-b-2 border-slate-600 rounded-sm"
@@ -135,18 +126,12 @@ const Credential = () => {
 						</label>
 						<input
 							required
-						<input
-							required
 							{...register("password", { required: true })}
 							type="password"
 							className={`bg-slate-200 border-b-2 border-slate-600 rounded-sm ${
 								errors.password ? "border-red-500" : ""
 							}`}
 							//"bg-slate-200 border-b-2 border-slate-600 rounded-sm"
-								errors.password ? "border-red-500" : ""
-							}`}
-							//"bg-slate-200 border-b-2 border-slate-600 rounded-sm"
-							placeholder="Enter your password"
 						/>
 						{errors.password && (
 							<p className="text-red-500 text-sm mt-1">
