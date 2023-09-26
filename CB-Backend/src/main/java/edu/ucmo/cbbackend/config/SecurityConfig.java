e3808838359946ef14d4f225b4d12da6478895b4
Ian Worley <ian@ianworley.xyz>
2023-09-25 19:23:48 -0500

    Add JWT support to log in and grab data from the backend will need to cite sources. Will close issue #14 and #11

--- CB-Backend/src/main/java/edu/ucmo/cbbackend/config/SecurityConfig.java
+++ CB-Backend/src/main/java/edu/ucmo/cbbackend/config/SecurityConfig.java
@@ -1,7 +1,13 @@
 package edu.ucmo.cbbackend.config;
 
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+import edu.ucmo.cbbackend.model.RsaKeyProperties;
 import edu.ucmo.cbbackend.service.UserDetailsService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
@@ -9,10 +15,12 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.configurers.userdetails.DaoAuthenticationConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -20,15 +28,28 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
+    private final RsaKeyProperties rsaKeys;
 
+    public SecurityConfig(RsaKeyProperties rsaKey) {
+        this.rsaKeys = rsaKey;
+    }
 
+    @Bean
+    JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withPublicKey(rsaKeys.publicKey()).build();
+    }
 
+    @Bean
+    JwtEncoder jwtEncoder() {
+        JWK jwk = new RSAKey.Builder(rsaKeys.publicKey()).privateKey(rsaKeys.privateKey()).build();
+        JWKSource<SecurityContext> jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+        return new NimbusJwtEncoder(jwks);
+    }
 
-
-        @Bean
-        public UserDetailsService userDetailsService() {
-            return new UserDetailsService();
-        }
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new UserDetailsService();
+    }
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
@@ -37,20 +58,18 @@ public class SecurityConfig {
 
     @Bean
     public DaoAuthenticationConfigurer daoAuthenticationConfigurer() {
-            DaoAuthenticationConfigurer dao = new DaoAuthenticationConfigurer(userDetailsService());
-            dao.passwordEncoder(passwordEncoder());
+        DaoAuthenticationConfigurer dao = new DaoAuthenticationConfigurer(userDetailsService());
+        dao.passwordEncoder(passwordEncoder());
 
-            return dao;
+        return dao;
 
     }
-
 
 
     @Bean()
     @Order(Ordered.HIGHEST_PRECEDENCE)
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf(csrf -> csrf.disable())
-                .formLogin(withDefaults()) // force form login for all requests
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/swagger-ui/**").permitAll() // permit all requests to swagger-ui
                         .requestMatchers("/v3/api-docs/**").permitAll()
@@ -59,7 +78,9 @@ public class SecurityConfig {
                         .requestMatchers("/user").permitAll() // permit all requests to login
                         .anyRequest().authenticated() // all other requests require authentication
                 )
-
+                .oauth2ResourceServer(oAuth -> oAuth.jwt(withDefaults()))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .httpBasic(withDefaults())
                 .build();
 
     }
